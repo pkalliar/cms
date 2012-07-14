ea003aa18a90b206ad6f089354d7e890ab110a01
unknown <tripoli@tripoli-HP.(none)>
2012-07-14 16:21:10 +0200

    init

---
+++ app/controllers/Application.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import java.util.Map;
+
+import org.codehaus.jackson.node.ArrayNode;
+import org.codehaus.jackson.node.ObjectNode;
+
+import com.avaje.ebean.Expr;
+import com.avaje.ebean.Page;
+
+import models.Contact;
+import models.Task;
+import play.*;
+import play.data.Form;
+import play.libs.Json;
+import play.mvc.*;
+
+import views.html.*;
+
+public class Application extends Controller {
+	
+	static Form<Task> taskForm = form(Task.class);
+  
+  public static Result index() {
+	return ok("Hello world");
+    //return ok(index.render("Your new application is ready."));
+  }
+  
+  public static Result tasks() {
+	  return ok(
+	    views.html.index.render(Task.all(), taskForm)
+	  );
+	}
+  
+  public static Result newTask() {
+	  Form<Task> filledForm = taskForm.bindFromRequest();
+	  if(filledForm.hasErrors()) {
+	    return badRequest(
+	      views.html.index.render(Task.all(), filledForm)
+	    );
+	  } else {
+	    Task.create(filledForm.get());
+	    return redirect(routes.Application.tasks());  
+	  }
+	}
+  
+  public static Result deleteTask(Long id) {
+	  Task.delete(id);
+	  return redirect(routes.Application.tasks());
+	}
+  
+
+  
+}
